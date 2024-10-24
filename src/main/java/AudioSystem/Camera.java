985e4ba5e46f62246bd10a760d681566880a1c9d
dark41Kree <90441919+dark41Kree@users.noreply.github.com>
2024-10-25 00:49:11 +0300

    commit 25 oct 2024 00_49

--- src/main/java/AudioSystem/Camera.java
+++ src/main/java/AudioSystem/Camera.java
@@ -1,50 +1,47 @@
 package AudioSystem;
 
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-
 public class Camera {
     private String nume;
     private int volume;
-    public Difuzor[] difuzor=new Difuzor[5];
-    public int nrdifuzoare=0;
+    public Difuzor[] difuzor = new Difuzor[5];
+    public int nrdifuzoare = 0;
 
 
-    public Camera(String nume, int volume){
+    public Camera(String nume, int volume) {
         setNume(nume);
         setVolume(volume);
     }
 
-    private void setNume(String nume){
-        this.nume=nume;
+    private void setNume(String nume) {
+        this.nume = nume;
     }
-    public String getNume(){
+
+    public String getNume() {
         return this.nume;
     }
-    private void setVolume(int volume){
-        this.volume=volume;
 
+    private void setVolume(int volume) {
+        this.volume = volume;
     }
-    public int getVolume(){
+
+    public int getVolume() {
         return this.volume;
     }
 
-
-    public void changeNumecamera(Camera camera,String nume){
-        camera.nume=nume;
+    public void changeNumecamera(Camera camera, String nume) {
+        camera.nume = nume;
     }
 
-    public void changeVolumecamera(Camera camera,int volume){
-        camera.volume=volume;
+    public void changeVolumecamera(Camera camera, int volume) {
+        camera.volume = volume;
     }
 
-    public void addDifuzor(String poz, String frecv){
-        if(nrdifuzoare<=4) {
-            difuzor[nrdifuzoare]=new Difuzor(poz,frecv);
-            nrdifuzoare=nrdifuzoare+1;
+    public void addDifuzor(String poz, String frecv) {
+        if (nrdifuzoare <= 4) {
+            difuzor[nrdifuzoare] = new Difuzor(poz, frecv);
+            nrdifuzoare = nrdifuzoare + 1;
         }
 
     }
-
 
 }
--- src/main/java/View/AudioSystem_View.java
+++ src/main/java/View/AudioSystem_View.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class AudioSystem_View extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader Fcasa = new FXMLLoader(Audio_System_Controller.class.getResource("Casa_form.fxml"));
+        FXMLLoader Fcasa = new FXMLLoader(Audio_System_Controller.class.getResource("Audio_System.fxml"));
         Scene scene = new Scene(Fcasa.load(), 420, 440);
         stage.setTitle("Fereastra pentru initializarea sistemului audio!");
         stage.setScene(scene);
